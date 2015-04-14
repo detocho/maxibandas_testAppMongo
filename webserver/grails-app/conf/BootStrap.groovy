6aeb954baa6d7ce2f33f8b0335e5645215f8e36e
davidpazgtz764 <dpaz@Air-de-David.lan>
2015-04-14 17:23:20 -0500

    Se agrega un registro a produccion

--- webserver/grails-app/conf/BootStrap.groovy
+++ webserver/grails-app/conf/BootStrap.groovy
@@ -11,6 +11,17 @@ class BootStrap {
 
         production{
 
+            if (TestUser.count() == 0) {
+                def user01 = new TestUser(
+                        name:'User Registrado1',
+                        email:'user10@detocho.com.mx'
+
+                )
+
+                user01.save()
+
+            }
+
         }
 
         development{
